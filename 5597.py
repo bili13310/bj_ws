dc9e7a40ac2bb13a9c2c5c8760a78eaa07eb020c
bili13310 <dlgusdnr1622@gmail.com>
2023-11-10 18:07:58 +0900

    1110

--- 5597.py
+++ 5597.py
@@ -1,11 +1,16 @@
+import sys
+
 N = []
-M = [i+1 for i in range(5)]
+M = [i+1 for i in range(30)]
+F = [j+1 for j in range(30)]
 
-for i in range(3):
-    N.append(int(input()))
+for i in range(28):
+    N.append(int(sys.stdin.readline()))
 
-for j in M:
-    for k in range(3):
-        if N[k] == M[j]:
-            M.remove(k)
-            print(M)
+for j in range(len(N)):
+    for k in F:
+        if N[j] == M[k-1]:
+            F.remove(k)
+
+for i in F:
+    print(i)
