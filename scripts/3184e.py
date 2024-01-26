8914eaa7c52cbe09a74b0050ed8f4343810df209
bili13310 <dlgusdnr1622@gmail.com>
2024-01-26 14:35:33 +0900

    0126

--- scripts/3184e.py
+++ scripts/3184e.py
@@ -1,10 +1,10 @@
 from collections import deque
 
 r, c = map(int, input().split())
-field = [list(input()) for _ in range(r)]
+ultari = [list(input()) for _ in range(r)]
 views = [[0] * c for _ in range(r)]
 dx = [0, 0, 0, 1, -1]
-dy = [0, 1, -1, 0 ,0]
+dy = [0, 1, -1, 0, 0]
 wolves = 0
 sheep = 0
 
@@ -16,26 +16,27 @@ def bfs(y, x):
     while deq:
         y, x = deq.popleft()
         for k in range(5):
-            ny, nx = y+dy[k], x+dx[k]
+            nx, ny = x+dx[k], y+dy[k]
 
             if 0 <= ny < r and 0 <= nx < c and views[ny][nx] == 0:
-                if field[ny][nx] == '#':
+                if ultari[ny][nx] == '#':
                     continue
-                elif field[ny][nx] == 'v':
+                elif ultari[ny][nx] == 'v':
                     v += 1
-                elif field[ny][nx] == 'o':
+                elif ultari[ny][nx] == 'o':
                     o += 1
                 deq.append((ny, nx))
                 views[ny][nx] = 1
     return o, v
-
+        
 deq = deque()
 for i in range(r):
     for j in range(c):
-        if field[i][j] != '#' and views[i][j] == 0:
+        if ultari[i][j] != '#' and views[i][j] == 0:
             s, w = bfs(i, j)
             if s > w:
                 sheep += s
-            elif s <= w:
+            else:
                 wolves += w
 print(sheep, wolves)
+
