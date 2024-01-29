c035cf953ffd0d70493f14f050847ab615230d35
bili13310 <dlgusdnr1622@gmail.com>
2024-01-29 13:45:02 +0900

    0129

--- scripts/test.py
+++ scripts/test.py
@@ -1,41 +1,23 @@
 from collections import deque
 
-r, c = map(int, input().split())
-field = [list(input()) for _ in range(r)]
-views = [[0] * c for _ in range(r)]
-dx = [0, 0, 0, 1, -1]
-dy = [0, 1, -1, 0 ,0]
-wolves = 0
-sheep = 0
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+field = [[0] * n for _ in range(n)]
+d = [(-2,-1), (-2,1), (0,-2), (0,2), (2,-1), (2,1)]
+
 
 def bfs(y, x):
     deq.append((y, x))
-    o = 0
-    v = 0
+    field[y][x] = 0
 
     while deq:
         y, x = deq.popleft()
-        for k in range(5):
-            ny, nx = y+dy[k], x+dx[k]
-
-            if 0 <= ny < r and 0 <= nx < c and views[ny][nx] == 0:
-                if field[ny][nx] == '#':
-                    continue
-                elif field[ny][nx] == 'v':
-                    v += 1
-                elif field[ny][nx] == 'o':
-                    o += 1
+        for dy, dx in d:
+            ny, nx = y+dy, x+dx
+            if 0 <= ny < n and 0 <= nx < n and field[ny][nx] == 0:
                 deq.append((ny, nx))
-                views[ny][nx] = 1
-    return o, v
+                field[ny][nx] = field[y][x] + 1
 
 deq = deque()
-for i in range(r):
-    for j in range(c):
-        if field[i][j] != '#' and views[i][j] == 0:
-            s, w = bfs(i, j)
-            if s > w:
-                sheep += s
-            elif s <= w:
-                wolves += w
-print(sheep, wolves)
+bfs(r1, c1)
+print(-1 if field[r2][c2] == 0 else field[r2][c2])
