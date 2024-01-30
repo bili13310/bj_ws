0dbe1332e5cbd7be4e6735631b3612f3903e90ce
bili13310 <dlgusdnr1622@gmail.com>
2024-01-30 16:00:33 +0900

    0130

--- scripts/24445e.py
+++ scripts/24445e.py
@@ -1,31 +1,33 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 n, m, r = map(int, input().split())
 field = [[] for _ in range(n+1)]
-views = [0 for _ in range(n+1)]
+visited = [0] * (n+1)
 count = 1
 
-for _ in range(n):
+for _ in range(m):
     u, v = map(int, input().split())
     field[u].append(v)
     field[v].append(u)
 
 def bfs(x):
-    global count
+    global count   
     deq.append(x)
-    views[x] = 1
+    visited[x] = 1
 
     while deq:
         x = deq.popleft()
-        field[x].sort()
-        for k in field[x]:
-            if views[k] == 0:
-                count += 1
-                views[k] = count
-                deq.append(k)
+        field[x].sort(reverse=True)
 
+        for k in field[x]:
+            if visited[k] == 0:
+                count += 1
+                visited[k] = count
+                deq.append(k)
 
 deq = deque()
 bfs(r)
-for i in views[1:]:
+for i in visited[1:]:
     print(i)
